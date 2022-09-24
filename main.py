4ea2c240d15a74af3cdbc7bb893b7e72e5371737
vismay <vismayjainjain2002@gmail.com>
2022-09-24 15:19:40 +0530

    basic codes of how api

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 from fastapi import FastAPI
+
 app = FastAPI()
-
- 
-
 
 @app.get("/")
 async def root():
     return {"message":"welcome to my api"}
 
+# defining type of parameter
+# @app.get("/items/{item_id}")
+# async def item(item_id: int):
+#     return {"item_id":item_id}
 
-@app.get("/post")
-def get_post():
-    return {"data":"This is your posts"}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get("/items/")
+async def read_items(skip: int = 0 , limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id": item_id}
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str , q:str |None = None , short: bool = False):
+#     item = {"item_id",item_id}
+#     if q:
+#         item.update({"q":q})
+#     if not short:
+#         item.update(
+#             {"description":"This is amazing item which has long description"}
+#         )
+#     return item
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
